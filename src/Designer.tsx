244ac683761f74fc61c6c2b6a9d913b4fff7bd73
Jhensen Aquino <47582827+Jhensen-Aquino@users.noreply.github.com>
2024-01-12 12:29:53 -0500

    Update Designer.tsx

--- src/Designer.tsx
+++ src/Designer.tsx
@@ -99,7 +99,8 @@ function App() {
         <span style={{ margin: "0 1rem" }}>/</span>
         <label style={{ width: 180 }}>
           Load Template
-          <input id="LoadTemplate" type="file" accept="application/json" onChange={(e) => handleLoadTemplate(e, designer.current)} />
+{/*           <input id="LoadTemplate" type="file" accept="application/json" onChange={(e) => handleLoadTemplate(e, designer.current)} /> */}
+          <input id="LoadTemplate" type="text" placeholder="Enter JSON data here" onChange={(e) => handleLoadTemplate(e.target, designer.current)} />
         </label>
         <span style={{ margin: "0 1rem" }}>/</span>
         <button id="DownloadTemplate" onClick={onDownloadTemplate}>Download Template</button>
